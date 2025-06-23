a00f3132d6904d59cf3d5c9f5907243fd70fe967
Nathan <nathanenguehard@gmail.com>
2025-06-24 01:17:09 +0200

    07 : custom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
-  return ( 
-    <div>
-     <p className="text-purple-700">
-      hello world
-     </p>
-    </div>
-     
+  return (
+    <main className="p-4">
+      <section className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated" className="border border-black">
+            I am a button
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="I am an input"  />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="I am a textarea"  />
+        </div>
+        <div className="flex items-center gap-x-2">
+          <Checkbox />
+        </div>
+      </section>
+    </main>
   );
-
-};
+}
