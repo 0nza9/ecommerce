abf96524b0817a209dcc1e47060b4e83fb12c02d
0nza9 <154357247+0nza9@users.noreply.github.com>
2025-07-12 13:48:14 +0200

    Update page.tsx

--- src/app/(home)/contact/page.tsx
+++ src/app/(home)/contact/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
     return (
         <div style={{ textAlign: "center", padding: "2rem" }}>
-            <h1>Contact Page</h1>
+            <h1>Mes contacts</h1>
             <div style={{ display: "flex", justifyContent: "center", gap: "2rem", flexWrap: "wrap" }}>
                 <img 
                     src="/cf5e04b3cd7fabef7a3167b99250e017.jpg" 
