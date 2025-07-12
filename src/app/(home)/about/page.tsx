2a909758774c0d4ab58ac76d8b00963d5a65470a
0nza9 <154357247+0nza9@users.noreply.github.com>
2025-07-12 13:33:38 +0200

    Update page.tsx

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -1,10 +1,15 @@
 const Page = () => {
     return (
-        <div>
-            blablabla
+        <div style={{ textAlign: "center", padding: "2rem" }}>
+            <h1>À propos</h1>
+            <p>blablabla</p>
+            <img 
+                src="/images/bdaa88c9edba6d3af2c25c5b32791ac0.jpg" 
+                alt="Abstrait" 
+                style={{ maxWidth: "100%", height: "auto", borderRadius: "12px" }}
+            />
         </div>
     );
-}
-
+};
 
 export default Page;
