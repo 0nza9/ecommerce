1a8fb844e0c5c38f770577e055c12d574ed93eaa
0nza9 <154357247+0nza9@users.noreply.github.com>
2025-07-12 13:38:01 +0200

    Update page.tsx

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -1,7 +1,6 @@
 const Page = () => {
     return (
         <div style={{ textAlign: "center", padding: "2rem" }}>
-            <h1>À propos</h1>
             <p>blablabla</p>
             <img 
                 src="/bdaa88c9edba6d3af2c25c5b32791ac0.jpg" 
