b4ecb199dee67d55cf8ceffbe5a69208d7c11d90
0nza9 <154357247+0nza9@users.noreply.github.com>
2025-07-12 14:57:00 +0200

    Update page.tsx

--- src/app/(home)/pricing/page.tsx
+++ src/app/(home)/pricing/page.tsx
@@ -1,10 +1,20 @@
 const Page = () => {
-    return (
-        <div>
-            pricing page 
-        </div>
-    );
-}
+  return (
+    <div className="relative min-h-screen">
+      
+      <div className="absolute inset-0 -z-10">
+        <img
+          src="d82f759105d6d95940420f255f95c649.jpg" 
+          alt="Background"
+          className="w-full h-full object-cover opacity-10 blur-sm"
+        />
+      </div>
 
+      <div className="p-10 text-gray-800 text-3xl font-light">
+        Pricing Page
+      </div>
+    </div>
+  );
+};
 
 export default Page;
